4d99de0747c9c0ec500460b5aee3f393eb3720b4
JUNTSURU <132868464+JUNTSURU@users.noreply.github.com>
2024-04-10 10:10:38 +0900

    課題提出(kadai_016 bookapp)

---
+++ php-book-app/index.php
@@ -0,0 +1,42 @@
+<!-- トップページ -->
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>書籍管理アプリ</title>
+
+    <!-- google fontsの読み込み -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- ヘッダー -->
+<header>
+  <nav>
+    <a href="index.php">書籍管理アプリ</a>
+  </nav>
+</header>
+
+<!-- メイン -->
+<main>
+  <article class="home">
+    <h1>書籍管理アプリ</h1>
+    <p>『PHPとデータベースを連携しよう』成果物</p>
+    <a href="read.php" class="btn">書籍一覧</a>
+  </article>
+</main>
+
+<!-- フッター -->
+<footer>
+  <p class="copyright">&copy; 書籍管理アプリ All rights reserved.</p>
+</footer>
+
+</body>
+
+</html>
